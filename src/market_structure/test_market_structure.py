79cb2a5569d21d807e4d8b1413e07e87a0629f32
Junior-Hebert D <djunibert@yahoo.fr>
2026-07-04 11:23:01 -0400

    Ajout du détecteur CHOCH bullish et bearish

--- src/market_structure/test_market_structure.py
+++ src/market_structure/test_market_structure.py
@@ -3,6 +3,7 @@ import pandas as pd
 from src.market_structure.swing_detector import SwingDetector
 from src.market_structure.trend_detector import TrendDetector
 from src.market_structure.bos_detector import BOSDetector
+from src.market_structure.choch_detector import CHOCHDetector
 
 
 df = pd.read_csv("data/features/market_macro/1h/gold.csv")
@@ -10,19 +11,19 @@ df = pd.read_csv("data/features/market_macro/1h/gold.csv")
 df = SwingDetector(window=2).detect(df)
 df = TrendDetector().detect(df)
 df = BOSDetector().detect(df)
+df = CHOCHDetector().detect(df)
 
 print(
     df[
         [
             "datetime",
             "close",
-            "last_swing_high",
-            "last_swing_low",
             "trend",
             "bos",
-            "bos_bullish",
-            "bos_bearish",
-            "bos_strength",
+            "bos_direction",
+            "choch",
+            "choch_direction",
+            "last_choch_direction",
         ]
-    ].tail(40)
+    ].tail(60)
 )
